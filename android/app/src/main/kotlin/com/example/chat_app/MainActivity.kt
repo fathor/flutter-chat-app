7f2816fa58ca4567b2d7b237ac8f898efb053ae7
onkz <onkzmugiwara@gmail.com>
2023-12-13 14:42:09 +0700

    “first”

---
+++ android/app/src/main/kotlin/com/example/chat_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.onkz.chatApp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:chat_app/screens/auth_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+        backgroundColor: Colors.grey,
+        buttonTheme: ButtonTheme.of(context).copyWith(
+          buttonColor: Colors.brown,
+          textTheme: ButtonTextTheme.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      ),
+      home: const AuthScreen(),
+    );
+  }
+}
